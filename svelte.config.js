9e2c6cba27ff875e86858c15b42810096349c665
moddroid94 <ilsecco94@gmail.com>
2025-08-17 21:55:45 +0200

    added cloudflare pages adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,16 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { } };
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
+};
 
 export default config;
