319e3faeb9108fe7d458671f8c9f6e93d7317c91
moddroid94 <ilsecco94@gmail.com>
2025-08-19 10:59:11 +0200

    added addons sources file

---
+++ src/lib/data/addons.js
@@ -0,0 +1,236 @@
+export const addons=[
+    {
+        "addon": "A.N.T Landscape",
+        "type": "Landscape Generator",
+        "img_url": "",
+        "link": "BUILT IN"
+    },
+    {
+        "addon": "Archimesh",
+        "type": "Add Architectural Primitive Mesh",
+        "img_url": "",
+        "link": "BUILT IN"
+    },
+    {
+        "addon": "Asset Bridge",
+        "type": "Integrate External Library",
+        "img_url": "",
+        "link": "https://github.com/strike-digital/asset_bridge"
+    },
+    {
+        "addon": "Auto Terrainer",
+        "type": "Geometry Node Terrain Generator",
+        "img_url": "",
+        "link": "https://unicornhunter.gumroad.com/l/AutoTerrainer"
+    },
+    {
+        "addon": "Bird Generator",
+        "type": "Self Explanatory",
+        "img_url": "",
+        "link": "https://blenderesse.gumroad.com/l/birdgenerator"
+    },
+    {
+        "addon": "BIS",
+        "type": "Material Library & Storage",
+        "img_url": "",
+        "link": "https://bis.interplanety.org/"
+    },
+    {
+        "addon": "Blender Kit",
+        "type": "Asset Library",
+        "img_url": "",
+        "link": "https://www.blenderkit.com/addons/"
+    },
+    {
+        "addon": "Blender to Pixels",
+        "type": "Compositor Based Pixel Art Filter",
+        "img_url": "",
+        "link": "https://astropulse.gumroad.com/l/BlenderToPixels?a=632791219"
+    },
+    {
+        "addon": "Blender Universe",
+        "type": "Asset & Nodes Library",
+        "img_url": "",
+        "link": "https://bakeduniverse.gumroad.com/l/bbps"
+    },
+    {
+        "addon": "BlenRig6",
+        "type": "Rig Any Character With Guide",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/blenrig"
+    },
+    {
+        "addon": "Blign",
+        "type": "Alignment & Distribution Tool",
+        "img_url": "",
+        "link": "https://github.com/Jack-Cooke/Blign"
+    },
+    {
+        "addon": "Book Generator",
+        "type": "Self Explanatory",
+        "img_url": "",
+        "link": "https://ow3d.de/bookGen"
+    },
+    {
+        "addon": "Buildify",
+        "type": "Powerful Building Generator",
+        "img_url": "",
+        "link": "https://paveloliva.gumroad.com/l/buildify"
+    },
+    {
+        "addon": "Bystedts Blender Baker",
+        "type": "Advanced Texture Baker",
+        "img_url": "",
+        "link": "https://3dbystedt.gumroad.com/l/JAqLT"
+    },
+    {
+        "addon": "Camera Cull Pro V2",
+        "type": "Real Time Camera Culling ( Dynamically Removes Geometry Not Visible In Camera)",
+        "img_url": "",
+        "link": "https://blenderlabs.gumroad.com/l/cameracullprov2"
+    },
+    {
+        "addon": "Cobweb Generator",
+        "type": "Self Explanatory",
+        "img_url": "",
+        "link": "https://vilemduha.gumroad.com/l/ZWBtg"
+    },
+    {
+        "addon": "Cracks and Damage Maker",
+        "type": "Self Explanatory",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/cracks-and-damage-maker-add-on"
+    },
+    {
+        "addon": "Deep Bump",
+        "type": "Make Bump / Normal Map For Any Geometry",
+        "img_url": "",
+        "link": "https://github.com/HugoTini/DeepBump"
+    },
+    {
+        "addon": "Drop It",
+        "type": "Drop An Object (Without Enable Physics)",
+        "img_url": "",
+        "link": "https://andreasaust.gumroad.com/l/drop_it"
+    },
+    {
+        "addon": "Dust Particles+",
+        "type": "Procedural Dust Generator",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/dust-particles"
+    },
+    {
+        "addon": "Easy PBR",
+        "type": "Link AmbientCG Library",
+        "img_url": "",
+        "link": "https://codeofart.gumroad.com/l/EasyPBR"
+    },
+    {
+        "addon": "Fantasytree",
+        "type": "Fantasy Tree Generator",
+        "img_url": "",
+        "link": "https://rc12.gumroad.com/l/fantasytree"
+    },
+    {
+        "addon": "Geocables",
+        "type": "Create Procedural Cables",
+        "img_url": "",
+        "link": "https://amanbairwal.gumroad.com/l/GeoCables"
+    },
+    {
+        "addon": "Geometry Node Pipes",
+        "type": "Self Explanatory",
+        "img_url": "",
+        "link": "https://raywakui.gumroad.com/l/pcxni?a=632791219"
+    },
+    {
+        "addon": "Gscatter",
+        "type": "Vegetation Scatter",
+        "img_url": "",
+        "link": "https://www.graswald3d.com/gscatter"
+    },
+    {
+        "addon": "JRemesher",
+        "type": "Fast Remesh Geometry",
+        "img_url": "",
+        "link": "https://github.com/jayanam/jremesh-tools"
+    },
+    {
+        "addon": "Maps Modeler Importer",
+        "type": "Import Google Maps Models",
+        "img_url": "",
+        "link": "https://github.com/eliemichel/MapsModelsImporter"
+    },
+    {
+        "addon": "Mossify",
+        "type": "Add Procedural Moss",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/mossify"
+    },
+    {
+        "addon": "Node Wrangler",
+        "type": "Node Shortcut Helper",
+        "img_url": "",
+        "link": "BUILT IN"
+    },
+    {
+        "addon": "Plant Library",
+        "type": "Self-Explanatory + Scattering Plugin",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/the-plant-library/?ref=794"
+    },
+    {
+        "addon": "Procedural Traffic",
+        "type": "Procedural Traffic Generator",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/procedural-traffic"
+    },
+    {
+        "addon": "Quicksnap",
+        "type": "Snap Objects To Other Objects!",
+        "img_url": "",
+        "link": "https://github.com/JulienHeijmans/quicksnap"
+    },
+    {
+        "addon": "Real Snow",
+        "type": "Add Real Snow To Any Scene",
+        "img_url": "",
+        "link": "BUILT IN"
+    },
+    {
+        "addon": "Sapling Tree Gen",
+        "type": "Generate Trees",
+        "img_url": "",
+        "link": "BUILT IN"
+    },
+    {
+        "addon": "Send 2 UE",
+        "type": "Automatically Send Most Of The Scene To Unreal Engine",
+        "img_url": "",
+        "link": "https://www.youtube.com/watch?v=4JT9pmYE8uI"
+    },
+    {
+        "addon": "Simplicage",
+        "type": "Deformable Mesh Cage Helper",
+        "img_url": "",
+        "link": "https://morelewd.gumroad.com/l/simplicage"
+    },
+    {
+        "addon": "Surface Add-on",
+        "type": "Create Geometry Displacement With Alpha Texture",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/surface-add-on-for-blender-1200-displacement-textures-bundle/?ref=794"
+    },
+    {
+        "addon": "UV Squares",
+        "type": "Better UV Square Ordering",
+        "img_url": "",
+        "link": "https://blendermarket.com/products/uv-squares"
+    },
+    {
+        "addon": "Quixel Bridge Importer",
+        "type": "Import Quixel Asset From Bridge - Requires Manual Files Copy, Do Not Install The Zip",
+        "img_url": "",
+        "link": "https://help.quixel.com/hc/en-us/articles/360007480698-User-Guide-Megascans-Plugin-for-Blender"
+    }
+]
