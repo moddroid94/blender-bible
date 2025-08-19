ba0723a6beaac1288d8ffc860f1ee65cb4232440
moddroid94 <ilsecco94@gmail.com>
2025-08-19 11:16:25 +0200

    added link card to repo to contribute page

---
+++ src/routes/contribute/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	
+</script>
+
+<div class="flex w-full bg-base-200 m-4 p-4 rounded-md border-1 border-accent/20 justify-center gap-4">
+    <a href="https://github.com/moddroid94/blender-bible">
+        <div class="card bg-base-100 w-96 shadow-sm cursor-pointer p-4">
+            <span>Collaborate on Github:</span>
+            <br>
+            <img alt="" src="https://gh-card.dev/repos/moddroid94/blender-bible.svg">
+        </div>
+    </a>
+</div>
