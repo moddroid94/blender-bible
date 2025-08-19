87f0328f1d2bd84f38c1c65dcc3b9fa489032e07
moddroid94 <ilsecco94@gmail.com>
2025-08-19 10:44:30 +0200

    added home page links to pages, just for fun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,50 @@
 	
 </script>
 
+<div class="flex w-full bg-base-200 m-4 p-4 rounded-md border-1 border-accent/20 justify-center gap-4">
+    <a href="/tutorials">
+        <div class="card bg-base-100 w-96 shadow-sm cursor-pointer" >
+            <figure class="px-10 pt-10 hover:scale-105 transition-transform">
+                <img
+                src="/image.png"
+                alt="Shoes"
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tutorials</h2>
+                <p>A list of curated tutorials</p>
+                
+            </div>
+        </div>
+    </a>
+    <a href="/channels">
+        <div class="card bg-base-100 w-96 shadow-sm cursor-pointer">
+            <figure class="px-10 pt-10 hover:scale-105 transition-transform">
+                <img
+                src="/channels.png"
+                alt="Shoes"
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Channels</h2>
+                <p>A list of curated educational channels</p>
+                
+            </div>
+        </div>
+    </a>
+    <a href="/sites">
+        <div class="card bg-base-100 w-96 shadow-sm cursor-pointer">
+            <figure class="px-10 pt-10 hover:scale-105 transition-transform">
+                <img
+                src="/tools.png"
+                alt="Shoes"
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Asset Sites</h2>
+                <p>A collection of free asset sites for evey need</p>
+                
+            </div>
+        </div>
+    </a>
+</div>
